dcaccfa21a0e3b56ad8d2806eee1e7fc65979c51
khalil daoud <khalil.daoud.18@cnu.edu>
2020-12-04 14:22:26 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="refresh" content="0; url=home.html" />
+  <link rel="icon" type="image/png" href="favicon.png">
+  <title>Loading...</title>
+</head>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript"> window.location.href = "home.html"</script>
+
+  </body>
+</html>
